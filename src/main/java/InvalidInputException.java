29b7d1c943f1fffb819a5e968cebab54fab0da07
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 15:25:26 +0200

    Update InvalidInputException.java

--- src/main/java/InvalidInputException.java
+++ src/main/java/InvalidInputException.java
@@ -1,2 +1,34 @@
 package PACKAGE_NAME;public class InvalidInputException {
 }
+public class Calculator {
+
+    // Метод додавання
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    // Метод віднімання
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Метод множення
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Метод ділення
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Помилка: Ділення на нуль!");
+        }
+        return a / b;
+    }
+
+    // Метод для перевірки вхідних значень
+    public void validateInput(double a, double b) throws InvalidInputException {
+        if (a < 0 || b < 0) {
+            throw new InvalidInputException("Помилка: Вхідні значення не повинні бути меншими за нуль!");
+        }
+    }
+}
